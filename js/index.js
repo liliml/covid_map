e21cd07939a965c606461f25f1f37e3849f6ebb7
lilianml <lilianml@uw.edu>
2025-02-11 15:41:06 -0800

    working on js for custom portion of lab

--- js/index.js
+++ js/index.js
@@ -9,41 +9,48 @@ const map = new mapboxgl.Map({
 );
 
 async function geojsonFetch() { 
-    let response = await fetch('assets/state_data.geojson');
-    let stateData = await response.json();
+    let response = await fetch('assets/wa-covid-data-102521.geojson');
+    let wa_covid_data = await response.json();
 
     map.on('load', function loadingData() {
         // add layer
         // add legend
-        map.addSource('state_data', {
+        map.addSource('wa-covid-data-102521', {
             type: 'geojson',
-            data: stateData
+            data: wa_covid_data
         });
         // console.log("got here");
         
         map.addLayer({
-            'id': 'stateData-layer',
+            'id': 'wa_covid_data-layer',
             'type': 'fill',
-            'source': 'state_data',
+            'source': 'wa-covid-data-102521',
             'paint': {
                 'fill-color': [
                     'step',
-                    ['get', 'density'],
+                    ['get', 'fullyVaxPer10k'],
                     '#FFEDA0',   // stop_output_0
-                    10,          // stop_input_0
+                    3000,          // stop_input_0
                     '#FED976',   // stop_output_1
-                    20,          // stop_input_1
+                    3500,          // stop_input_1
                     '#FEB24C',   // stop_output_2
-                    50,          // stop_input_2
+                    4000,          // stop_input_2
                     '#FD8D3C',   // stop_output_3
-                    100,         // stop_input_3
+                    4500,         // stop_input_3
                     '#FC4E2A',   // stop_output_4
-                    200,         // stop_input_4
+                    5000,         // stop_input_4
                     '#E31A1C',   // stop_output_5
-                    500,         // stop_input_5
+                    5500,         // stop_input_5
                     '#BD0026',   // stop_output_6
-                    1000,        // stop_input_6
-                    "#800026"    // stop_output_7
+                    6000,        // stop_input_6
+                    "#800026",    // stop_output_7
+                    6500,        // stop_input_7
+                    "#e13b26",    // stop_output_8
+                    7000,        // stop_input_8
+                    "#ad0955",    // stop_output_9
+                    7500,        // stop_input_9
+                    "#86013e",    // stop_output_10
+                    
                 ],
                 'fill-outline-color': '#BBBBBB',
                 'fill-opacity': 0.7
@@ -53,24 +60,30 @@ async function geojsonFetch() {
 
     
     const layers = [
-        '0-9',
-        '10-19',
-        '20-49',
-        '50-99',
-        '100-199',
-        '200-499',
-        '500-999',
-        '1000 and more'
+        '3000-3499',
+        '3500-3999',
+        '4000-4499',
+        '4500-4999',
+        '5000-5499',
+        '5500-5999',
+        '6000-6499',
+        '6500-6999',
+        '7000-7499',
+        '7500-7999',
+        '8000 or more'
     ];
     const colors = [
-        '#FFEDA070',
-        '#FED97670',
-        '#FEB24C70',
-        '#FD8D3C70',
-        '#FC4E2A70',
-        '#E31A1C70',
-        '#BD002670',
-        '#80002670'
+        '#FFEDA0', 
+        '#FED976', 
+        '#FEB24C',
+        '#FD8D3C', 
+        '#FC4E2A',  
+        '#E31A1C',  
+        '#BD0026', 
+        "#800026",  
+        "#e13b26",  
+        "#ad0955",  
+        "#86013e"
     ];
     
     const legend = document.getElementById('legend');
@@ -92,7 +105,7 @@ async function geojsonFetch() {
     
     map.on('mousemove', ({point}) => {
         const state = map.queryRenderedFeatures(point, {
-            layers: ['stateData-layer']
+            layers: ['wa_covid_data-layer']
         });
         document.getElementById('text-description').innerHTML = state.length ?
             `<h3>${state[0].properties.name}</h3><p><strong><em>${state[0].properties.density}</strong> people per square mile</em></p>` :
