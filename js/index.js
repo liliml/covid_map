a2100019021500cb06281acdd140468c46afc523
lilianml <lilianml@uw.edu>
2025-02-12 12:43:56 -0800

    fixed colors in the map and key and made lower key wider on the map

--- js/index.js
+++ js/index.js
@@ -30,27 +30,25 @@ async function geojsonFetch() {
                     'step',
                     ['get', 'fullyVaxPer10k'],
                     '#FFEDA0',   // stop_output_0
-                    3000,          // stop_input_0
-                    '#FED976',   // stop_output_1
-                    3500,          // stop_input_1
+                    3000,          // stop_input_0 use color above if fullvax 3000 <
+                    '#FED976',   // stop_output_1 
+                    3500,          // stop_input_1 use color above if 3000 <= fullyvax < 3500
                     '#FEB24C',   // stop_output_2
-                    4000,          // stop_input_2
+                    4000,          // stop_input_2 use color above if 3500 <= fullyvax < 4000
                     '#FD8D3C',   // stop_output_3
-                    4500,         // stop_input_3
+                    4500,         // stop_input_3 use color above if 4000 <= fullyvax < 4500
                     '#FC4E2A',   // stop_output_4
-                    5000,         // stop_input_4
+                    5000,         // stop_input_4 use color above if 4500 <= fullyvax < 5000
                     '#E31A1C',   // stop_output_5
-                    5500,         // stop_input_5
+                    5500,         // stop_input_5 use color above if 5000 <= fullyvax < 5500
                     '#BD0026',   // stop_output_6
-                    6000,        // stop_input_6
+                    6000,        // stop_input_6 use color above if 5500 <= fullyvax < 6000
                     "#800026",    // stop_output_7
-                    6500,        // stop_input_7
-                    "#2a006f",    // stop_output_8
-                    7000,        // stop_input_8
-                    "#5000d2",    // stop_output_9
-                    7500,        // stop_input_9
-                    "#000000",    // stop_output_10
-                    
+                    6500,        // stop_input_7 use color above if 6000 <= fullyvax < 6500
+                    "#5000d2",    // stop_output_8 
+                    7000,        // stop_input_8 use color above if 6500 <= fullyvax < 7000
+                    "#2a006f",    // stop_output_9 
+                                  // use color above if 7000 <= fullyvax 
                 ],
                 'fill-outline-color': '#BBBBBB',
                 'fill-opacity': 0.7
@@ -60,6 +58,7 @@ async function geojsonFetch() {
 
     
     const layers = [
+        'less then 3000',
         '3000-3499',
         '3500-3999',
         '4000-4499',
@@ -68,9 +67,7 @@ async function geojsonFetch() {
         '5500-5999',
         '6000-6499',
         '6500-6999',
-        '7000-7499',
-        '7500-7999',
-        '8000 or more'
+        '7000 or more'
     ];
     const colors = [
         '#FFEDA0', 
@@ -81,13 +78,12 @@ async function geojsonFetch() {
         '#E31A1C',  
         '#BD0026', 
         "#800026",  
-        "#2a006f",  
         "#5000d2",  
-        "#000000"
+        "#2a006f"  
     ];
     
     const legend = document.getElementById('legend');
-    legend.innerHTML = "<b>Population Density<br>(people/sq.mi.)</b><br><br>";
+    legend.innerHTML = "<b>Number of Fully Vaccinated People<br>(people per 10k within the county)</b><br><br>";
     
     layers.forEach((layer, i) => {
         const color = colors[i];
